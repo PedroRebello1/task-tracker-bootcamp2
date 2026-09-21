c480d7fb339bf77360df9da72c4b9f1cb3f40d2f
PEDRO REBELLO BORGES DE BARROS <E80004428@cassi.com.br>
2026-09-21 17:51:43 -0300

    Adiciona os métodos para Usuário

--- implementacao/src/modelos/usuario.py
+++ implementacao/src/modelos/usuario.py
@@ -19,4 +19,42 @@ class Usuario:
         # Preferencia de tema. É armazenado na conta.
         self.tema = tema if tema in configuracao.TEMAS else configuracao.TEMA_PADRAO
 
+    # criacao
+    @classmethod
+    def criar(cls, id, nome, login, senha, perfil=configuracao.PERFIL_COMUM):
+        """Cria o usuario ja embaralhando a senha."""
+        return cls(id=id, nome=nome, login=login.strip().lower(),
+                   senha_hash=generate_password_hash(senha), perfil=perfil)
 
+    # checagens
+    def senha_confere(self, senha):
+        return check_password_hash(self.senha_hash, senha)
+
+    @property
+    def eh_admin(self):
+        return self.perfil == configuracao.PERFIL_ADMIN
+
+    # conversao
+    def para_dict(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "login": self.login,
+            "senha": self.senha_hash,
+            "perfil": self.perfil,
+            "tema": self.tema,
+        }
+
+    @classmethod
+    def de_dict(cls, dados):
+        return cls(
+            id=dados["id"],
+            nome=dados["nome"],
+            login=dados["login"],
+            senha_hash=dados["senha"],
+            perfil=dados.get("perfil", configuracao.PERFIL_COMUM),
+            tema=dados.get("tema", configuracao.TEMA_PADRAO),
+        )
+
+    def __repr__(self):
+        return "<Usuario #%s %r (%s)>" % (self.id, self.login, self.perfil)
